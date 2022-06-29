92581a29bc2e9ee5c8fe71baeb4bea340941b21d
Oleksandr Lutsenko <oleksandr.l@siteplus.com>
2022-06-29 13:31:14 +0300

    init

---
+++ SnusOperations/SnusOperations.cs
@@ -0,0 +1,17 @@
+﻿namespace SnusOperations
+{
+    public static class SnusOperations
+    {
+        public const string Snus = "snus";
+
+        public static string GetSnus()
+        {
+            return Snus;
+        }
+
+        public static void PrintSnus()
+        {
+            Console.WriteLine(Snus);
+        }
+    }
+}
